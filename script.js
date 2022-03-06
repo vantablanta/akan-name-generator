d0b7b23b01dde1aa048427ec48026654dd03bf02
vanta blanta <vantablanta@gmail.com>
2022-03-06 16:53:54 +0300

    change the js to call classes for the various akans based on gender

--- script.js
+++ script.js
@@ -47,54 +47,114 @@ function collectData() {
         modal.classList.add('modal-display')
         modal.textContent = "Wow! Are you from the future. Click logo to retry "
         footer.classList.add('footer-hide')    
-
     }
     else if (isNaN(d) || gender == ''){
         // warning
-        console.log('You havent entered your gender or birthday')
+        form.classList.add('form-hidden')
+        text.classList.add('hide-description')
+        modal.classList.add('modal-display')
+        modal.textContent = "You havent entered either your gender or birthday. Click logo to retry"
+        footer.classList.add('footer-hide')    
     }
     else if (gender == "male" && day == 0){
-
-        console.log(maleAkans[0])
+        form.classList.add('form-hidden')
+        text.classList.add('hide-description')
+        modal.classList.add('modal-display')
+        modal.textContent = `You were born on ${days[0]}.Your Akan Name is : ${maleAkans[0]}`
+        footer.classList.add('footer-hide')  
     }
     else if (gender == 'female' && day  == 0){
-        console.log(femaleAkans[0])
+        form.classList.add('form-hidden')
+        text.classList.add('hide-description')
+        modal.classList.add('modal-display')
+        modal.textContent = `You were born on ${days[0]}.Your Akan Name is : ${femaleAkans[0]}`
+        footer.classList.add('footer-hide')  
     }
     else if (gender == 'male' && day  == 1){
-        console.log(maleAkans[1])
+        form.classList.add('form-hidden')
+        text.classList.add('hide-description')
+        modal.classList.add('modal-display')
+        modal.textContent = `You were born on ${days[1]}.Your Akan Name is : ${maleAkans[1]}`
+        footer.classList.add('footer-hide')
     }
     else if (gender == 'female' && day  == 1){
-        console.log(femaleAkans[1])
+        form.classList.add('form-hidden')
+        text.classList.add('hide-description')
+        modal.classList.add('modal-display')
+        modal.textContent = `You were born on ${days[1]}.Your Akan Name is : ${femaleAkans[1]}`
+        footer.classList.add('footer-hide')
+
     }
     else if (gender == 'male' && day  == 2){
-        console.log(maleAkans[2])
+        form.classList.add('form-hidden')
+        text.classList.add('hide-description')
+        modal.classList.add('modal-display')
+        modal.textContent = `You were born on ${days[2]}.Your Akan Name is : ${maleAkans[2]}`
+        footer.classList.add('footer-hide')
     }
     else if (gender == 'female' && day  == 2){
-        console.log(femaleAkans[2])
+        form.classList.add('form-hidden')
+        text.classList.add('hide-description')
+        modal.classList.add('modal-display')
+        modal.textContent = `You were born on ${days[2]}.Your Akan Name is : ${femaleAkans[2]}`
+        footer.classList.add('footer-hide')
     }
     else if (gender == 'male' && day  == 3){
-        console.log(maleAkans[3])
+        form.classList.add('form-hidden')
+        text.classList.add('hide-description')
+        modal.classList.add('modal-display')
+        modal.textContent = `You were born on ${days[3]}.Your Akan Name is : ${maleAkans[3]}`
+        footer.classList.add('footer-hide')
     }
     else if (gender == 'female' && day  == 3){
-        console.log(femaleAkans[3])
+        form.classList.add('form-hidden')
+        text.classList.add('hide-description')
+        modal.classList.add('modal-display')
+        modal.textContent = `You were born on ${days[3]}.Your Akan Name is : ${femaleAkans[3]}`
+        footer.classList.add('footer-hide')
     }
     else if (gender == 'male' && day  == 4){
-        console.log(maleAkans[4])
+        form.classList.add('form-hidden')
+        text.classList.add('hide-description')
+        modal.classList.add('modal-display')
+        modal.textContent = `You were born on ${days[4]}.Your Akan Name is : ${maleAkans[3]}`
+        footer.classList.add('footer-hide')
     }
     else if (gender == 'female' && day  == 4){
-        console.log(femaleAkans[4])
+        form.classList.add('form-hidden')
+        text.classList.add('hide-description')
+        modal.classList.add('modal-display')
+        modal.textContent = `You were born on ${days[4]}.Your Akan Name is : ${femaleAkans[4]}`
+        footer.classList.add('footer-hide')
     }
     else if (gender == 'male' && day  == 5){
-        console.log(maleAkans[5])
+        form.classList.add('form-hidden')
+        text.classList.add('hide-description')
+        modal.classList.add('modal-display')
+        modal.textContent = `You were born on ${days[5]}.Your Akan Name is : ${maleAkans[5]}`
+        footer.classList.add('footer-hide')
     }
     else if (gender == 'female' && day  == 5){
-        console.log(femaleAkans[5])
+        form.classList.add('form-hidden')
+        text.classList.add('hide-description')
+        modal.classList.add('modal-display')
+        modal.textContent = `You were born on ${days[5]}.Your Akan Name is : ${femaleAkans[5]}`
+        footer.classList.add('footer-hide')
     }
     else if (gender == 'male' && day  == 6){
+        form.classList.add('form-hidden')
+        text.classList.add('hide-description')
+        modal.classList.add('modal-display')
+        modal.textContent = `You were born on ${days[6]}.Your Akan Name is : ${maleAkans[6]}`
+        footer.classList.add('footer-hide')
         console.log(maleAkans[6])
     }
     else if (gender == 'female' && day  == 6){
-        console.log(femaleAkans[6])
+        form.classList.add('form-hidden')
+        text.classList.add('hide-description')
+        modal.classList.add('modal-display')
+        modal.textContent = `You were born on ${days[6]}.Your Akan Name is : ${femaleAkans[6]}`
+        footer.classList.add('footer-hide')
     }
 
     
