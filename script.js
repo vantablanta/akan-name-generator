1bb3f3511369e58c1a7f6d81f3bb873b3e4c21d9
vanta blanta <vantablanta@gmail.com>
2022-03-06 17:35:49 +0300

    nav link outside modal added

--- script.js
+++ script.js
@@ -87,6 +87,11 @@ function collectData() {
         modal.classList.add('modal-display')
         modal.textContent = `You were born on ${days[0]}.Your Akan Name is : ${femaleAkans[0]}.`
         footer.classList.add('footer-hide')
+        a.classList.add("link-style")
+        link =document.createTextNode("Back Home")    
+        a.appendChild(link); 
+        a.href = "index.html"; 
+        modal.appendChild(a);
     }
     else if (gender == 'male' && day == 1) {
         form.classList.add('form-hidden')
@@ -94,6 +99,11 @@ function collectData() {
         modal.classList.add('modal-display')
         modal.textContent = `You were born on ${days[1]}.Your Akan Name is : ${maleAkans[1]}.`
         footer.classList.add('footer-hide')
+        a.classList.add("link-style")
+        link =document.createTextNode("Back Home")    
+        a.appendChild(link); 
+        a.href = "index.html"; 
+        modal.appendChild(a);
     }
     else if (gender == 'female' && day == 1) {
         form.classList.add('form-hidden')
@@ -101,6 +111,11 @@ function collectData() {
         modal.classList.add('modal-display')
         modal.textContent = `You were born on ${days[1]}.Your Akan Name is : ${femaleAkans[1]}.`
         footer.classList.add('footer-hide')
+        a.classList.add("link-style")
+        link =document.createTextNode("Back Home")    
+        a.appendChild(link); 
+        a.href = "index.html"; 
+        modal.appendChild(a);
 
     }
     else if (gender == 'male' && day == 2) {
@@ -109,6 +124,11 @@ function collectData() {
         modal.classList.add('modal-display')
         modal.textContent = `You were born on ${days[2]}.Your Akan Name is : ${maleAkans[2]}.`
         footer.classList.add('footer-hide')
+        a.classList.add("link-style")
+        link =document.createTextNode("Back Home")    
+        a.appendChild(link); 
+        a.href = "index.html"; 
+        modal.appendChild(a);
     }
     else if (gender == 'female' && day == 2) {
         form.classList.add('form-hidden')
@@ -116,6 +136,11 @@ function collectData() {
         modal.classList.add('modal-display')
         modal.textContent = `You were born on ${days[2]}.Your Akan Name is : ${femaleAkans[2]}.`
         footer.classList.add('footer-hide')
+        a.classList.add("link-style")
+        link =document.createTextNode("Back Home")    
+        a.appendChild(link); 
+        a.href = "index.html"; 
+        modal.appendChild(a);
     }
     else if (gender == 'male' && day == 3) {
         form.classList.add('form-hidden')
@@ -123,6 +148,11 @@ function collectData() {
         modal.classList.add('modal-display')
         modal.textContent = `You were born on ${days[3]}.Your Akan Name is : ${maleAkans[3]}.`
         footer.classList.add('footer-hide')
+        a.classList.add("link-style")
+        link =document.createTextNode("Back Home")    
+        a.appendChild(link); 
+        a.href = "index.html"; 
+        modal.appendChild(a);
     }
     else if (gender == 'female' && day == 3) {
         form.classList.add('form-hidden')
@@ -130,6 +160,11 @@ function collectData() {
         modal.classList.add('modal-display')
         modal.textContent = `You were born on ${days[3]}.Your Akan Name is : ${femaleAkans[3]}.`
         footer.classList.add('footer-hide')
+        a.classList.add("link-style")
+        link =document.createTextNode("Back Home")    
+        a.appendChild(link); 
+        a.href = "index.html"; 
+        modal.appendChild(a);
     }
     else if (gender == 'male' && day == 4) {
         form.classList.add('form-hidden')
@@ -137,6 +172,11 @@ function collectData() {
         modal.classList.add('modal-display')
         modal.textContent = `You were born on ${days[4]}.Your Akan Name is : ${maleAkans[3]}.`
         footer.classList.add('footer-hide')
+        a.classList.add("link-style")
+        link =document.createTextNode("Back Home")    
+        a.appendChild(link); 
+        a.href = "index.html"; 
+        modal.appendChild(a);
     }
     else if (gender == 'female' && day == 4) {
         form.classList.add('form-hidden')
@@ -144,6 +184,11 @@ function collectData() {
         modal.classList.add('modal-display')
         modal.textContent = `You were born on ${days[4]}.Your Akan Name is : ${femaleAkans[4]}.`
         footer.classList.add('footer-hide')
+        a.classList.add("link-style")
+        link =document.createTextNode("Back Home")    
+        a.appendChild(link); 
+        a.href = "index.html"; 
+        modal.appendChild(a);
     }
     else if (gender == 'male' && day == 5) {
         form.classList.add('form-hidden')
@@ -151,6 +196,11 @@ function collectData() {
         modal.classList.add('modal-display')
         modal.textContent = `You were born on ${days[5]}.Your Akan Name is : ${maleAkans[5]}.`
         footer.classList.add('footer-hide')
+        a.classList.add("link-style")
+        link =document.createTextNode("Back Home")    
+        a.appendChild(link); 
+        a.href = "index.html"; 
+        modal.appendChild(a);
     }
     else if (gender == 'female' && day == 5) {
         form.classList.add('form-hidden')
@@ -158,6 +208,11 @@ function collectData() {
         modal.classList.add('modal-display')
         modal.textContent = `You were born on ${days[5]}.Your Akan Name is : ${femaleAkans[5]}.`
         footer.classList.add('footer-hide')
+        a.classList.add("link-style")
+        link =document.createTextNode("Back Home")    
+        a.appendChild(link); 
+        a.href = "index.html"; 
+        modal.appendChild(a);
     }
     else if (gender == 'male' && day == 6) {
         form.classList.add('form-hidden')
@@ -166,6 +221,11 @@ function collectData() {
         modal.textContent = `You were born on ${days[6]}.Your Akan Name is : ${maleAkans[6]}.`
         footer.classList.add('footer-hide')
         console.log(maleAkans[6])
+        a.classList.add("link-style")
+        link =document.createTextNode("Back Home")    
+        a.appendChild(link); 
+        a.href = "index.html"; 
+        modal.appendChild(a);
     }
     else if (gender == 'female' && day == 6) {
         form.classList.add('form-hidden')
@@ -173,6 +233,11 @@ function collectData() {
         modal.classList.add('modal-display')
         modal.textContent = `You were born on ${days[6]}.Your Akan Name is : ${femaleAkans[6]}.`
         footer.classList.add('footer-hide')
+        a.classList.add("link-style")
+        link =document.createTextNode("Back Home")    
+        a.appendChild(link); 
+        a.href = "index.html"; 
+        modal.appendChild(a);
     }
 }
 
