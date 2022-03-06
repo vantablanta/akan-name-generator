dccc9d0a049f2001012a22b83491098ee8603274
vanta blanta <vantablanta@gmail.com>
2022-03-06 14:58:48 +0300

    makes sure user cant eneter a date in the future,  makes sure user knows they havent enterded a date

--- script.js
+++ script.js
@@ -1,21 +1,21 @@
 //akan data
 let maleAkans = [
-    "Sunday: Kwasi",
-    "Monday: Kwadwo",
-    "Tuesday: Kwabena",
-    "Wednesday: Kwaku",
-    "Thursday: Yaw",
-    "Friday: Kofi",
-    "Saturday: Kwame"
+    "Kwasi",
+    "Kwadwo",
+    "Kwabena",
+    "Kwaku",
+    "Yaw",
+    "Kofi",
+    "Kwame"
 ]
 let femaleAkans = [
-    "Sunday: Akosua",
-    "Monday: Adwoa",
-    "Tuesday: Abenaa",
-    "Wednesday: Akua",
-    "Thursday:  Yaa",
-    "Friday: Afua",
-    "Saturday: Ama"
+    "Akosua",
+    "Adwoa",
+    "Abenaa",
+    "Akua",
+    "Yaa",
+    "Afua",
+    "Ama"
 ]
 let days = [
     'Sunday',
@@ -30,17 +30,64 @@ let days = [
 
 //add event listener
 let inputAction = document.getElementById('btn').addEventListener('click', collectData)
-//find index day
 function collectData() {
-    //collect input date
     let birthday = document.getElementById('date').value
-    //convert to date format
     const d = new Date(birthday)
-    // get the day in index
+    const today = new Date()
     let day = d.getDay()
-    console.log(day)
-    // get the gender
+    let gender = document.getElementById('gender').value;
+    if (d > today){
+        console.log("Seems like you havent been born yet")
+    }
+    else if (gender == ''){
+        console.log('You havent entered your gender')
+    }
+    else if (gender == "male" && day == 0){
+        console.log(maleAkans[0])
+    }
+    else if (gender == 'female' && day  == 0){
+        console.log(femaleAkans[0])
+    }
+    else if (gender == 'male' && day  == 1){
+        console.log(maleAkans[1])
+    }
+    else if (gender == 'female' && day  == 1){
+        console.log(femaleAkans[1])
+    }
+    else if (gender == 'male' && day  == 2){
+        console.log(maleAkans[2])
+    }
+    else if (gender == 'female' && day  == 2){
+        console.log(femaleAkans[2])
+    }
+    else if (gender == 'male' && day  == 3){
+        console.log(maleAkans[3])
+    }
+    else if (gender == 'female' && day  == 3){
+        console.log(femaleAkans[3])
+    }
+    else if (gender == 'male' && day  == 4){
+        console.log(maleAkans[4])
+    }
+    else if (gender == 'female' && day  == 4){
+        console.log(femaleAkans[4])
+    }
+    else if (gender == 'male' && day  == 5){
+        console.log(maleAkans[5])
+    }
+    else if (gender == 'female' && day  == 5){
+        console.log(femaleAkans[5])
+    }
+    else if (gender == 'male' && day  == 6){
+        console.log(maleAkans[6])
+    }
+    else if (gender == 'female' && day  == 6){
+        console.log(femaleAkans[6])
+    }
+
+    
 }
+
 
 
 
